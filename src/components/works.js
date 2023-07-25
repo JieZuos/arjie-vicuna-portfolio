3bda337636ed7d5181123c9f0171829018bd1364
JieZuos <arjie.vicuna@gmail.com>
2023-07-25 11:18:32 +0800

    img attempt

--- src/components/works.js
+++ src/components/works.js
@@ -141,37 +141,42 @@ const GalleryComponent = () => {
         </div>
         <div className="item" data-list="ui">
         <Tilt options={TiltImage} >
-          <a href="https://www.figma.com/proto/hoflO9eF0T1AYGwCK2NkNv/tailoredbuilding?page-id=0%3A1&type=design&node-id=1-2&t=NxxcZWoMR2Yp1aVf-0&scaling=min-zoom">
             <img src="/assets/images/ui/tailored.png" alt="ui" />
-          </a>
         </Tilt>
         </div>
         <div className="item" data-list="ui">
         <Tilt options={TiltImage} >
-          <a href="https://www.figma.com/proto/3XmeEx8OlMr1wMqpMqHJNe/SMART-BRAND-1?page-id=0%3A1&type=design&node-id=1-2&t=zCEqqKv5AcRcAC9T-0&scaling=min-zoom">
             <img src="/assets/images/ui/smart brand 1.png" alt="ui" />
-          </a>
         </Tilt>
         </div>
         <div className="item" data-list="ui">
         <Tilt options={TiltImage} >
-          <a href="https://www.figma.com/proto/SFtiVOdpZsYvxnNrXmucDN/SMART-BRAND-2?page-id=0%3A1&type=design&node-id=1-2&t=iq2uiCBtWDPStax9-0&scaling=min-zoom">
             <img src="/assets/images/ui/smart brand 2.png" alt="ui" />
-          </a>
         </Tilt>
         </div>
         <div className="item" data-list="ui">
         <Tilt options={TiltImage} >
-          <a href="https://www.figma.com/proto/2XN3t3c3Ui1lXbn2HxWvfR?page-id=0%3A1&type=design&node-id=1-2&t=7DX7jE9375GbcQmR-0&scaling=min-zoom">
             <img src="/assets/images/ui/smart brand 3.png" alt="ui" />
-          </a>
         </Tilt>
         </div>
         <div className="item" data-list="ui">
         <Tilt options={TiltImage} >
-          <a href="https://www.figma.com/proto/wOI1LII9t3BUlFesB5i6WM/Lead-Magnet?page-id=0%3A1&type=design&node-id=7-44&t=w7Q1q3o7Ncrz0gvn-0&scaling=min-zoom">
-            <img src="/assets/images/ui/modal.png" alt="ui" />
-          </a>
+            <img src="/assets/images/ui/modal 3.png" alt="ui" />
+        </Tilt>
+        </div>
+        <div className="item" data-list="ui">
+        <Tilt options={TiltImage} >
+            <img src="/assets/images/ui/modal 1.png" alt="ui" />
+        </Tilt>
+        </div>
+        <div className="item" data-list="ui">
+        <Tilt options={TiltImage} >
+            <img src="/assets/images/ui/modal 2.png" alt="ui" />
+        </Tilt>
+        </div>
+        <div className="item" data-list="ui">
+        <Tilt options={TiltImage} >
+            <img src="/assets/images/ui/modal 4.png" alt="ui" />
         </Tilt>
         </div>
       </div>
